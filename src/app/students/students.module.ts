62573c7a1736a249ed05500b054fc14cad7e5b20
Michael Gianfaldone <gmitch.dev@outlook.fr>
2017-12-27 23:18:56 +0100

    Added students module and students-listing component

---
+++ src/app/students/students.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentsListingComponent } from './students-listing/students-listing.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [StudentsListingComponent]
+})
+export class StudentsModule { }
